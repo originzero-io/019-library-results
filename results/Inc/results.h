80c33b942ac7ab9a06cc2e5ae1f3b1d9d05da1f1
fatihyazman <50661537+fatihyazman@users.noreply.github.com>
2022-12-17 16:25:24 +0300

    Modified: result_error_callback and Added items to results_enum_t

--- results/Inc/results.h
+++ results/Inc/results.h
@@ -14,31 +14,45 @@ extern "C" {
 
 #include <stdint.h>
 
+/* @formatter:off */
 typedef enum results_enum
 {
-	RESULT_SUCCESS,
-	RESULT_ERR_HEAP_MEM,
-	RESULT_ERR_TIMEOUT,
-	RESULT_ERR_SIZE,
-	RESULT_ERR_LEN,
-	RESULT_ERR_NULL,
-	RESULT_ERR_CRC,
-	RESULT_ERR_CHECKSUM,
-	RESULT_ERR_NOT_FOUND
-}results_enum_t;
+	/* SUCCESS */
+	RESULT_SUCCESS = 1,
+	RESULT_CONNECTED,
 
+/* ERRORS */
+	RESULT_ERR_UNKNOWN = -127,		/* Unknown error. */
+	RESULT_ERR_HEAP_MEM,
+	RESULT_ERR_TIMEOUT,				/* The operation timed out. */
+	RESULT_ERR_SIZE,
+	RESULT_ERR_NULL,				/* Function parameters or variables are null. */
+	RESULT_ERR_CRC, 				/* CRC error. */
+	RESULT_ERR_LEN, 				/* Length error. */
+	RESULT_ERR_CHECKSUM, 			/* checksum error. */
+	RESULT_ERR_QUIUE, 				/* Quiue added error. */
+	RESULT_ERR_NOTFOUND, 			/* Search was not found. */
+	RESULT_ERR_PACKING, 			/* Packaging has been done. */
+	RESULT_ERR_NOT_INIT, 			/* It didn't initialize. */
+	RESULT_ERR_SECTOR_NUMBER_BIG,		/* Sector number big. */
+	RESULT_ERR_VERIFY,				/* Verify error. */
+	RESULT_ERR_CHECK,				/* check error. */
+	RESULT_ERR_AUTO_ECC_GENERATION,	/* Auto ECC generation error. */
+	RESULT_ERR_FSM_STATUS,			/* FSM status error. */
+	RESULT_ERR_ENABLE,				/* Enable error. */
+	RESULT_ERR_INIT,				/* Initialize error. */
+	RESULT_ERR_ERASE				/* Erase error. */
+}results_enum_t;
+/* @formatter:on */
 
 typedef struct result_struct
 {
 	results_enum_t result;
 	uint8_t library_code;
 	uint8_t function_code;
-}result_struct_t;
+} result_struct_t;
 
-result_struct_t results_return(results_enum_t result, uint8_t library_code, uint8_t function_code);
-void result_error_callback(result_struct_t result);
-
-
+void result_error_callback(results_enum_t result, uint8_t library_code, uint8_t function_code);
 
 #ifdef __cplusplus
 }
--- results/Src/results.c
+++ results/Src/results.c
@@ -7,19 +7,8 @@
 
 #include "results.h"
 
-result_struct_t results_return(results_enum_t result, uint8_t library_code, uint8_t function_code)
-{
-	result_struct_t return_value;
 
-	return_value.result = result;
-	return_value.library_code = library_code;
-	return_value.function_code = function_code;
-
-	return return_value;
-}
-
-
-void result_error_callback(result_struct_t result)
+void result_error_callback(results_enum_t result, uint8_t library_code, uint8_t function_code)
 {
 
 }
