caec92c3beb473cade30ad5c1715122fae11f1ee
fatihyazman <50661537+fatihyazman@users.noreply.github.com>
2022-12-20 15:38:15 +0300

    added result_loop and filled and filled in result_error_callback

--- results/Inc/results.h
+++ results/Inc/results.h
@@ -14,6 +14,10 @@ extern "C" {
 
 #include <stdint.h>
 
+#define RESULT_IS_ERROR(v) (v < 0)
+
+#define RESULT_ERROR_BUFFER_COUNT 10
+
 /* @formatter:off */
 typedef enum results_enum
 {
@@ -23,38 +27,37 @@ typedef enum results_enum
 	RESULT_DISCONNECTED,
 
 
-/* ERRORS */
-	RESULT_ERR_UNKNOWN = -127,		/* Unknown error. */
+	/* ERRORS */
+	RESULT_ERR_UNKNOWN = -127,         /* Unknown error. */
 	RESULT_ERR_HEAP_MEM,
-	RESULT_ERR_TIMEOUT,				/* The operation timed out. */
-	RESULT_ERR_RECEIVE_TIMEOUT,		/* Receive timed out. */
-	RESULT_ERR_SIZE,
-	RESULT_ERR_NULL,				/* Function parameters or variables are null. */
-	RESULT_ERR_CRC, 				/* CRC error. */
-	RESULT_ERR_LEN, 				/* Length error. */
-	RESULT_ERR_CHECKSUM, 			/* checksum error. */
-	RESULT_ERR_QUIUE, 				/* Quiue added error. */
-	RESULT_ERR_NOTFOUND, 			/* Search was not found. */
-	RESULT_ERR_PACKING, 			/* Packaging has been done. */
-	RESULT_ERR_NOT_INIT, 			/* It didn't initialize. */
-	RESULT_ERR_SECTOR_NUMBER_BIG,		/* Sector number big. */
-	RESULT_ERR_VERIFY,				/* Verify error. */
-	RESULT_ERR_CHECK,				/* check error. */
-	RESULT_ERR_AUTO_ECC_GENERATION,	/* Auto ECC generation error. */
-	RESULT_ERR_FSM_STATUS,			/* FSM status error. */
-	RESULT_ERR_ENABLE,				/* Enable error. */
-	RESULT_ERR_INIT,				/* Initialize error. */
-	RESULT_ERR_ERASE,				/* Erase error. */
-	RESULT_ERR_CONNECT,
-	RESULT_ERR_NOTCONNECTED,
-	RESULT_ERR_PORT,
-	RESULT_ERR_LINK,
-	RESULT_ERR_TRANSMIT,
-	RESULT_ERR_LISTEN,
-	RESULT_ERR_BIND,
+	RESULT_ERR_TIMEOUT,                /* The operation timed out. */
+	RESULT_ERR_RECEIVE_TIMEOUT,        /* Receive timed out. */
+	RESULT_ERR_SIZE,                   /* Size error */
+	RESULT_ERR_NULL,                   /* Function parameters or variables are null. */
+	RESULT_ERR_CRC,                    /* CRC error. */
+	RESULT_ERR_LEN,                    /* Length error. */
+	RESULT_ERR_CHECKSUM,               /* checksum error. */
+	RESULT_ERR_QUIUE,                  /* Quiue added error. */
+	RESULT_ERR_NOTFOUND,               /* Search was not found. */
+	RESULT_ERR_PACKING,                /* Packaging has been done. */
+	RESULT_ERR_NOT_INIT,               /* It didn't initialize. */
+	RESULT_ERR_SECTOR_NUMBER_BIG,      /* Sector number big. */
+	RESULT_ERR_VERIFY,                 /* Verify error. */
+	RESULT_ERR_CHECK,                  /* check error. */
+	RESULT_ERR_AUTO_ECC_GENERATION,    /* Auto ECC generation error. */
+	RESULT_ERR_FSM_STATUS,             /* FSM status error. */
+	RESULT_ERR_ENABLE,                 /* Enable error. */
+	RESULT_ERR_INIT,                   /* Initialize error. */
+	RESULT_ERR_ERASE,                  /* Erase error. */
+	RESULT_ERR_CONNECTION,             /* Connection is error */
+	RESULT_ERR_NOTCONNECTED,			/* Connection is no*/
+	RESULT_ERR_PORT,                   /* Port is wrong */
+	RESULT_ERR_LINK,                   /* Ethernet link error*/
+	RESULT_ERR_TRANSMIT,               /* Transmit error */
+	RESULT_ERR_LISTEN,                 /* Listen error */
+	RESULT_ERR_BIND,                   /* Bind error */
 
 	/* STATE */
-
 	RESULT_IN_THE_PROCESS
 
 }results_enum_t;
@@ -65,9 +68,11 @@ typedef struct result_struct
 	results_enum_t result;
 	uint8_t library_code;
 	uint8_t function_code;
+	uint8_t is_loaded;
 } result_struct_t;
 
 void result_error_callback(results_enum_t result, uint8_t library_code, uint8_t function_code);
+void result_loop(void);
 
 #ifdef __cplusplus
 }
--- results/Src/results.c
+++ results/Src/results.c
@@ -7,8 +7,44 @@
 
 #include "results.h"
 
+static result_struct_t results_errors_quiue[RESULT_ERROR_BUFFER_COUNT];
+static uint8_t results_errors_quiue_index_counter;
 
 void result_error_callback(results_enum_t result, uint8_t library_code, uint8_t function_code)
 {
+	results_errors_quiue[results_errors_quiue_index_counter].result = result;
+	results_errors_quiue[results_errors_quiue_index_counter].library_code = library_code;
+	results_errors_quiue[results_errors_quiue_index_counter].function_code = function_code;
+	results_errors_quiue[results_errors_quiue_index_counter].is_loaded = 1;
 
+	if (RESULT_ERROR_BUFFER_COUNT < (++results_errors_quiue_index_counter))
+	{
+		results_errors_quiue_index_counter = 0;
+	}
 }
+
+void result_loop(void)
+{
+	static uint8_t results_errors_quiue_index_counter_loop, is_logged = 0, tried_count = 0;
+
+	do
+	{
+
+		if (1 == results_errors_quiue[results_errors_quiue_index_counter_loop].is_loaded)
+		{
+			/*@TODO LOG WRITE */
+
+			results_errors_quiue[results_errors_quiue_index_counter_loop].is_loaded = 0;
+			is_logged = 1;
+		}
+
+		tried_count++;
+
+		if (RESULT_ERROR_BUFFER_COUNT < (++results_errors_quiue_index_counter_loop))
+		{
+			results_errors_quiue_index_counter_loop = 0;
+		}
+
+	} while (0 == is_logged && RESULT_ERROR_BUFFER_COUNT > tried_count);
+}
+
